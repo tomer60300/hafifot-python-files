e49be049f21aa72c858fc1f1145a82d08920c225
Tomerc <tomer60300@gmail.com>
2025-08-21 18:27:40 +0300

    fix: Depth
    Change the depth format to start from 1 - immediate files and 2 or greater for sub-dirs
    
    Search not isn't support fetching directories, only files.

--- directory/search.py
+++ directory/search.py
@@ -6,22 +6,14 @@ from typing import Iterator
 def walk_with_depth(root: Path, depth: int) -> Iterator[Path]:
     """
     Yield files and dirs up to a given depth.
-    depth = 0 -> only the root folder
-    depth = 1 -> root + immediate files
+    depth = 1 -> immediate files
     depth = 2 -> include subfolders, etc.
     """
     root = root.resolve()
     top_depth = len(root.parts)
 
-    if depth == 0:
-        yield root
-        return
-
     for dirpath, dirnames, filenames in os.walk(root):
         current_depth = len(Path(dirpath).parts) - top_depth
-
-        # yield folder itself
-        yield Path(dirpath)
 
         # yield files inside
         for f in filenames:
