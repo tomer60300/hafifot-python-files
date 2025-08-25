93b2a7c3f7bc06f31042ce612dd293f27086eefc
Tomerc <tomer60300@gmail.com>
2025-08-25 10:03:47 +0300

    feature
    added archive call

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from config import parse_args
 import logging
 from logging_conf import configure_logging
-from directory.search import walk_with_depth
+from operation.search import walk_filesystem_by_filters, archive_files_by_filters
 
 if __name__ == "__main__":
     configure_logging("DEBUG")
@@ -10,9 +10,14 @@ if __name__ == "__main__":
     try:
         config = parse_args()
         logger.info("Parsed config: %s", config)
+        logger.debug(
+            "Special mode %s" % ("*Size* " if config.size != 0 else "") + ("*Archive* " if config.archive_name else ""))
 
-        for result in walk_with_depth(config.folder, config.depth,config.pattern,config.size):
-            print(f"{result} -- ",end="")
+        if config.archive_name:
+            archive_files_by_filters(config.archive_name,config.folder, config.depth, config.pattern, config.size)  # Todo call params missing
+        else:
+            for result in walk_filesystem_by_filters(config.folder, config.depth, config.pattern, config.size):
+                print(f"{result} -- ", end="")
     except Exception as ex:
         logger.critical("Fatal error: %s", ex, exc_info=True)
         raise
