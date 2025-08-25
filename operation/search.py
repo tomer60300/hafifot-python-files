e895d6e1d29478b379ee902d28cac50435bda564
Tomerc <tomer60300@gmail.com>
2025-08-25 10:03:58 +0300

    feature
    added archive logic

--- operation/search.py
+++ operation/search.py
@@ -2,9 +2,11 @@ import os
 from pathlib import Path
 from typing import Iterator
 import re
+from zipfile import ZipFile, ZIP_DEFLATED
 
+ZIP_FILE_NAME = "archive.zip"
 
-def walk_with_depth(root: Path, depth: int,pattern: re.Pattern, max_size: int=0) -> Iterator[Path]:
+def walk_filesystem_by_filters(root: Path, depth: int, pattern: re.Pattern, max_size: int=0) -> Iterator[Path]:
     """
     Yield files and dirs up to a given depth.
     depth = 1 -> immediate files
@@ -29,3 +31,15 @@ def walk_with_depth(root: Path, depth: int,pattern: re.Pattern, max_size: int=0)
         # prune dirs if max depth reached
         if current_depth >= depth - 1:
             dirnames.clear()  # don't go deeper
+
+
+
+def archive_files_by_filters(zip_path: Path,root: Path, depth: int, pattern: re.Pattern, max_size: int=0) -> ZipFile:
+
+
+    with ZipFile(zip_path, "w", compression=ZIP_DEFLATED) as archive_file:
+        for path_obj in walk_filesystem_by_filters(root=root, depth=depth, pattern=pattern, max_size=max_size):
+            if  path_obj.name not in archive_file.namelist():
+                archive_file.write(path_obj, arcname=path_obj.name)
+
+    return archive_file
